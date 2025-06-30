1208eeb9fb3e746a157a78a7cff9b727f8b1bea9
Emmy-S-300 <emmy.schonau@gmail.com>
2025-06-30 11:39:13 -0400

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+import unittest
+from app import app
+
+class FlaskAppTestCase(unittest.TestCase):
+  def setup(self):
+    
+    self.app - app.test_client()
+    self.app.testing = True
+
+  def test_home_page_status_code(self):
+    response = self.app.get('/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_home_page_content(self):
+    response = self.app.get('/')
+    self.assertIn(b"<title>", response.data)
+    
+if __name__ == '__main__':
+  unittest.main()
